2205792d5c609ffac7a0655641544f7e69a12e62
Ilyas Mohamed <zswaleh@ymail.com>
2023-04-27 10:44:41 -0500

    made a database

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE shopping_list (
+	id SERIAL PRIMARY KEY,
+	name varchar(80) NOT NULL,
+	quantity DECIMAL(10,2) NOT NULL,
+	unit varchar(20) NULL
+);
+
+INSERT INTO shopping_list ("name", "quantity", "unit")
+VALUES ('Apples', 5.3, 'lbs'), ('Milk', 2, 'Gallon')
